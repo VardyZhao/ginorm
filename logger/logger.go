23e3de395d12df9ba0cb2d34111a0e57722328cd
Vardy-zhao <vardy.zhao@lifebyte.io>
2024-12-24 10:52:07 +0800

    日志整改

--- logger/logger.go
+++ logger/logger.go
@@ -1,20 +1,21 @@
 package logger
 
 import (
+	"context"
 	"fmt"
 	"ginorm/config"
 	"ginorm/constant"
 	"ginorm/util"
-	"github.com/gin-gonic/gin"
 	"github.com/natefinch/lumberjack"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"log"
+	"os"
 )
 
 type loggerConfig struct {
 	Path       string `mapstructure:"path"`
-	MaxSize    int    `mapstructure:"mxx_size"`
+	MaxSize    int    `mapstructure:"max_size"`
 	MaxBackups int    `mapstructure:"max_backups"`
 	MaxAge     int    `mapstructure:"max_age"`
 	Compress   bool   `mapstructure:"compress"`
@@ -39,14 +40,16 @@ var (
 	LevelFatal  = zapcore.FatalLevel
 )
 
-// Load 初始化 Logger，将不同日志级别输出到不同文件
 func Load() {
 	if err := config.Conf.UnmarshalKey("log", &lc); err != nil {
 		log.Fatalf("Error unmarshaling databases config: %v", err)
 	}
 
-	infoCore := newCore(zapcore.InfoLevel)   // INFO 级别日志
-	errorCore := newCore(zapcore.ErrorLevel) // ERROR 级别日志
+	// INFO 级别日志核心，只记录 INFO 和 WARN 等较低级别日志
+	infoCore := newCore(zapcore.InfoLevel, zapcore.WarnLevel)
+
+	// ERROR 级别日志核心，只记录 ERROR 及以上级别的日志
+	errorCore := newCore(zapcore.ErrorLevel, zapcore.FatalLevel)
 
 	core := zapcore.NewTee(infoCore, errorCore)
 
@@ -80,13 +83,13 @@ func Flush() {
 func Log(level zapcore.Level, msg string, args ...interface{}) {
 	var fields []zap.Field
 	var dataMap map[string]any
-	var ctx *gin.Context
+	var ctx context.Context
 
 	for _, arg := range args {
 		switch v := arg.(type) {
 		case map[string]any:
 			dataMap = v
-		case *gin.Context:
+		case context.Context:
 			ctx = v
 		}
 	}
@@ -96,26 +99,30 @@ func Log(level zapcore.Level, msg string, args ...interface{}) {
 	}
 
 	if ctx != nil {
-		// 假设GetString方法存在
-		fields = append(fields, zap.String(constant.TraceId, ctx.GetString(constant.TraceId)))
+		fields = append(fields, zap.Any(constant.TraceId, ctx.Value(constant.TraceId)))
 	}
 
 	Writer.Log(level, msg, fields...)
 }
 
-func newCore(level zapcore.Level) zapcore.Core {
-	filename, ok := levelFileMap[level]
+func newCore(minLevel zapcore.Level, maxLevel zapcore.Level) zapcore.Core {
+	filename, ok := levelFileMap[minLevel]
 	if !ok {
 		filename = levelFileMap[zapcore.InfoLevel]
 	}
 	filename = util.GetAbsPath("/" + lc.Path + "/" + filename)
-	writer := zapcore.AddSync(&lumberjack.Logger{
+
+	// 文件日志输出
+	fileWriter := zapcore.AddSync(&lumberjack.Logger{
 		Filename:   filename,
-		MaxSize:    lc.MaxSize,    // 每个日志文件最大 10MB
-		MaxBackups: lc.MaxBackups, // 最多保留 5 个旧文件
-		MaxAge:     lc.MaxAge,     // 日志文件最长保留 30 天
-		Compress:   lc.Compress,   // 是否压缩旧日志文件
+		MaxSize:    lc.MaxSize,
+		MaxBackups: lc.MaxBackups,
+		MaxAge:     lc.MaxAge,
+		Compress:   lc.Compress,
 	})
+
+	// 控制台日志输出
+	consoleWriter := zapcore.AddSync(os.Stdout)
 
 	// 配置日志编码
 	encoderConfig := zap.NewProductionEncoderConfig()
@@ -123,5 +130,14 @@ func newCore(level zapcore.Level) zapcore.Core {
 	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder // 使用 ISO8601 格式化时间
 	encoder := zapcore.NewJSONEncoder(encoderConfig)
 
-	return zapcore.NewCore(encoder, writer, level)
+	// 设置 LevelEnabler 过滤级别
+	levelEnabler := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= minLevel && lvl <= maxLevel
+	})
+
+	// 使用 zapcore.NewTee，将文件和控制台输出合并并限制日志级别
+	return zapcore.NewTee(
+		zapcore.NewCore(encoder, fileWriter, levelEnabler),
+		zapcore.NewCore(encoder, consoleWriter, levelEnabler),
+	)
 }
